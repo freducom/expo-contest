e37fab0cbb10e3fc256f0e3a010beb1c22196cf7
Fredrik Rönnlund <freducom@users.noreply.github.com>
2016-05-19 12:33:31 +0200

    Update Application.java

--- src/main/java/expo/Application.java
+++ src/main/java/expo/Application.java
@@ -1,8 +1,11 @@
-//DO NOT TOUCH THIS FILE. YOU ONLY NEED TO TOUCH THE MyUI.java FILE
 package expo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * DO NOT TOUCH THIS FILE. YOU ONLY NEED TO TOUCH THE MyUI.java FILE
+ */
 
 @SpringBootApplication
 public class Application {
